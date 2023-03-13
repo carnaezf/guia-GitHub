55bb11626f950c9747ee72bca1476e145a9ca889
Colmeex <colmeex@gmail.com>
2023-03-13 15:56:06 -0400

    funcion binario y hexadecimal

--- index.js/index.js
+++ index.js/index.js
@@ -1,3 +1,8 @@
-function laCajaDePandora(numero) {
+function laCajaDePandora(num) {
   // proximamente escribiremos codigo aqui
+  if (num % 2 === 0) {
+    return num.toString(2);
+  } else return num.toString(16);
 }
+
+console.log(laCajaDePandora(454345));
