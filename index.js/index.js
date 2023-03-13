a5c32a49760ee5b475852171606029f0b83f841c
Nahir-Arrieta <nahir.arrieta@hotmail.com>
2023-03-13 19:45:00 -0300

    commit cambio funcion

--- index.js/index.js
+++ index.js/index.js
@@ -1,8 +1,9 @@
 function laCajaDePandora(num) {
   // proximamente escribiremos codigo aqui
-  if (num % 2 === 0) {
+  if (num % 4 === 0) {
     return num.toString(2);
   } else return num.toString(16);
 }
 
+console.log(20);
 console.log(laCajaDePandora(454345));
