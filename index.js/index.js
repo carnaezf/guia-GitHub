8a3f01b35f5afa4cce2f565fa6030ca9813d81ef
Cesar Arnaez Flores <c_arnaez.flores@hotmail.com>
2023-03-13 16:54:11 -0300

    Agregar funcion usuario carnaez

---
+++ index.js/index.js
@@ -0,0 +1,10 @@
+function laCajaDePandora(numero) {
+  // proximamente escribiremos codigo aqui
+  function laCajaDePandora(numero){
+    if(numero % 2 === 0){
+        return numero.toString(2);
+    }else{
+        return numero.toString(16);
+    }
+}
+}
